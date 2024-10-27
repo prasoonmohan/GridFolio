ecdbbf4633954495fe8f00020e1290a4a833de8a
Prasoon M <prasoon@aufaitux.com>
2024-10-27 17:23:50 +0530

    fix : Added navbar close when we click contact us btn

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -6,17 +6,20 @@ const activeRoute = ref("home");
 const route = useRoute();
 
 onMounted(() => {
-  activeRoute.value = route.name 
+  activeRoute.value = route.name
 });
 
 watch(() => {
-  activeRoute.value = route.name 
+  activeRoute.value = route.name
 });
 
 const isMenu = ref(false);
 
 const toggleMenu = () => {
   isMenu.value = !isMenu.value;
+};
+const handleClose = () => {
+  isMenu.value = false;
 };
 
 
@@ -25,25 +28,28 @@ const toggleMenu = () => {
 <template>
   <header class="header">
     <div :class="{ 'header-container': true, 'container': true, 'active': isMenu }">
-     <div class="logo-container">
-      <RouterLink to="/" class="logo-text">GridFolio</RouterLink>
-      <div @click="toggleMenu" :class="{ 'open': isMenu, 'hamburger-icon': true }" id="nav-icon3">
-  <span></span>
-  <span></span>
-  <span></span>
-  <span></span> 
-</div>
-     </div>
+      <div class="logo-container">
+        <RouterLink to="/" class="logo-text">GridFolio</RouterLink>
+        <div @click="toggleMenu" :class="{ 'open': isMenu, 'hamburger-icon': true }" id="nav-icon3">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
       <ul class="link-container">
-        <RouterLink @click="toggleMenu" to="/" class="links" :class="{ active: activeRoute === 'home' }">Home</RouterLink>
-        <RouterLink @click="toggleMenu" to="/work" class="links" :class="{ active: activeRoute === 'work' }">Works</RouterLink>
-        <RouterLink @click="toggleMenu" to="/about" class="links" :class="{ active: activeRoute === 'about' }">About</RouterLink>
-        <RouterLink @click="toggleMenu" to="/resume" class="links" :class="{ active: activeRoute === 'resume' }">Resume</RouterLink>
+        <RouterLink @click="handleClose" to="/" class="links" :class="{ active: activeRoute === 'home' }">Home
+        </RouterLink>
+        <RouterLink @click="handleClose" to="/work" class="links" :class="{ active: activeRoute === 'work' }">Works
+        </RouterLink>
+        <RouterLink @click="handleClose" to="/about" class="links" :class="{ active: activeRoute === 'about' }">About
+        </RouterLink>
+        <RouterLink @click="handleClose" to="/resume" class="links" :class="{ active: activeRoute === 'resume' }">Resume
+        </RouterLink>
       </ul>
-<RouterLink to="/contact" class="get-in">
-  <button class="btn get-in-touch">Get in touch</button>
-</RouterLink>
+      <RouterLink @click="handleClose" to="/contact" class="get-in">
+        <button class="btn get-in-touch">Get in touch</button>
+      </RouterLink>
     </div>
   </header>
 </template>
-
